05e4f223d52c3e1697a8905e4c74db7e319a31cd
Jackie Jr <jwysinger@gmail.com>
2023-11-13 06:51:56 -0600

    Up to creation of instance of phpmailer class

---
+++ forms/send-email.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message']; 
+
+require 'vendor/autoload.php'; 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP; 
+
+$mail = new PHPMailer(true); 
