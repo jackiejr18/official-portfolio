12c4ce07ffe2c78bc8302742f9500713c3d9e383
Jackie Jr <jwysinger@gmail.com>
2023-11-13 07:43:51 -0600

    Code for the SMTP server that host

--- forms/send-email.php
+++ forms/send-email.php
@@ -11,3 +11,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP; 
 
 $mail = new PHPMailer(true); 
+
+$mail->isSMTP();
+$mail->SMTPAuth = true; 
+
+$mail->Host = 'jackiejr1892@gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587; 
+
+$mail->Username = 'jackiejr1892@gmail.com';
+$mail->Password = 'password';
